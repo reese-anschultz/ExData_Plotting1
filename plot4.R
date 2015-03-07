56ced33fc7380e20edeb2323c2caed57f9a6d517
Reese Anschultz <reese@anschultz.org>
2015-03-07 12:31:15 -0800

    Added plot4.R and plot4.png. Fixed plot3's output to not be cropped in the PNG file

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 # Exploratory Data Analysis
 # Course Project 1
-# plot3
+# plot4
 # by Reese Anschultz
 
 library(lubridate)
@@ -10,7 +10,7 @@ zip_file<-"household_power_consumption.zip"
 data_file<-"household_power_consumption.txt"
 min_date<-dmy('01-02-2007')
 max_date<-dmy('02-02-2007')
-output_filename<-"plot3.png"
+output_filename<-"plot4.png"
 
 # Read the raw data in
 download.file(url,zip_file)
@@ -21,13 +21,28 @@ hpc<-read.csv(data_file,header=TRUE,sep=";",na.strings="?")
 hpc<-hpc[min_date<=dmy(hpc$Date) & dmy(hpc$Date) <=max_date,]
 
 png(filename=output_filename)
+par(mfcol=c(2,2))
 
-# Plot information
-#hist(hpc$Global_active_power,main="Global Active Power",lab="Global Active Power (kilowatts)",col="red")
+# Plot 2 information
+plot(hpc$Global_active_power,type="l",xaxt="n",xlab=NA,ylab="Global Active Power")
+axis(1,labels=c("Thu","Fri","Sat"),at=c(1,nrow(hpc)/2,nrow(hpc)))
+
+# Plot 3 information
+#hist(hpc$Global_active_power,main="Global Active Power",lab="Global Active Power",col="red")
 plot(hpc$Sub_metering_1,type="l",xaxt="n",xlab=NA,ylab="Energy sub metering",col="black")
 lines(hpc$Sub_metering_2,col="red")
 lines(hpc$Sub_metering_3,col="blue")
 axis(1,labels=c("Thu","Fri","Sat"),at=c(1,nrow(hpc)/2,nrow(hpc)))
 legend(x="topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lwd=c(2.5,2.5,2.5))
+
+# Plot 4-1 information
+plot(hpc$Voltage,type="l",xaxt="n",xlab="datetime",ylab="Voltage",col="black")
+axis(1,labels=c("Thu","Fri","Sat"),at=c(1,nrow(hpc)/2,nrow(hpc)))
+
+# Plot 4-2 information
+plot(hpc$Global_reactive_power,type="l",xaxt="n",xlab="datetime",ylab="Global_reactive_power",col="black")
+axis(1,labels=c("Thu","Fri","Sat"),at=c(1,nrow(hpc)/2,nrow(hpc)))
+
+# Make a persistent copy
 dev.off()
 
